9c3f025035f302bd25628948bb5f090ef596f790
Jahmelia Atkinson <jahmeliaatkinson@yahoo.com>
2020-11-25 16:21:08 +0000

    final

---
+++ springbootmvcdemo/src/main/java/com/tcs/springbootmvcdemo/interceptor/GuestInterceptor.java
@@ -0,0 +1,40 @@
+package com.tcs.springbootmvcdemo.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class GuestInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Called before handler");
+	request.setAttribute("fname", "Abhi");
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		
+
+		System.out.println("called after handler method request completion but before rendering");
+		modelAndView.addObject("lname", "chivate");
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+		System.out.println("after completion method of intercepotr");
+
+	}
+
+}
